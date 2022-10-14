5eac3424fe77031e8e743d1b8c39a79234cf7d46
frankosorioweb <osoriofranciscoj99@gmail.com>
2022-10-14 10:52:06 -0300

    El endpoint /paciente [POST] permite crear un paciente nuevo

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -10,16 +10,16 @@ use Illuminate\Support\Facades\Http;
 class PacienteController extends Controller
 {
 
-  private $id            = null;
-  private $nombres       = null;
-  private $apellidos     = null;
-  private $edad          = null;
-  private $sexo          = null;
-  private $documento     = null;
-  private $tipo_sangre   = null;
-  private $telefono      = null;
-  private $correo        = null;
-  private $direccion     = null;
+  private $id          = null;
+  private $nombres     = null;
+  private $apellidos   = null;
+  private $edad        = null;
+  private $sexo        = null;
+  private $documento   = null;
+  private $tipo_sangre = null;
+  private $telefono    = null;
+  private $correo      = null;
+  private $direccion   = null;
 
   /**
    * El endpoint /paciente/{id?} [GET] se utiliza para comunicarse con esta función y actuar de controlador
@@ -46,10 +46,40 @@ class PacienteController extends Controller
    * 
    * @return JsonResponse
    */
-  public function addNew(Request $request) {
-    return new JsonResponse([
-      "msg" => "Hello from /paciente [POST]"
-    ]);
+  public function addNew(Request $request)
+  {
+    // Verificamos si los parámetros del nuevo paciente son válidos
+    $verify_parameters = $this->verify_add_new_paciente_parameters($request);
+    if ($verify_parameters instanceof JsonResponse) {
+      return $verify_parameters;
+    }
+
+    // Construimos la url de la api correspondiente para insertar un nuevo paciente
+    $pacientes_post_api = $this->get_pacientes_api();
+    $paciente_http_body_parameters = $this->get_paciente_http_body($request);
+
+    // Enviamos la petición [POST] a JSONPLACEHOLDER y obtenemos el response
+    $response = $paciente_http_body_parameters->post($pacientes_post_api);
+
+    return new JsonResponse(
+      $response->json(),
+      Response::HTTP_CREATED
+    );
+  }
+
+  private function get_paciente_http_body(Request $request) {
+    return Http::withBody(json_encode([
+      "id" => intval($request->get('id')),
+      "nombres" => $request->get('nombres'),
+      "apellidos" => $request->get('apellidos'),
+      "edad" => intval($request->get('edad')),
+      "sexo" => $request->get('sexo'),
+      "dni" => intval($request->get('dni')),
+      "tipo_sangre" => $request->get('tipo_sangre'),
+      "telefono" => intval($request->get('telefono')),
+      "correo" => $request->get('correo'),
+      "direccion" => $request->get('direccion')
+    ]), "application/json");
   }
 
   /**
@@ -59,7 +89,7 @@ class PacienteController extends Controller
    * 
    * @return JsonResponse|bool
    */
-  private function verify_find_one_by_id($request, $response)
+  private function verify_find_one_by_id(Request $request, $response)
   {
     $has_paciente_id_param = !empty($request->id);
     $paciente_not_found = $has_paciente_id_param && empty($response->json());
@@ -74,16 +104,52 @@ class PacienteController extends Controller
   }
 
   /**
-   * Retorna la api de pacientes de JSONPLACEHOLDER, en función se si se desea buscar obtener
-   * ltodos os pacientes o buscar uno en particular a partir de su respectivo id
+   * Retorna la api de pacientes de JSONPLACEHOLDER, permite construir la api en función se si se desea obtener
+   * todos los pacientes, buscar uno en particular a partir de su respectivo id o simplemente retorna la api original
+   * cuando el parámetro request es null
    * 
    * @return String
    */
-  private function get_pacientes_api(Request $request)
+  private function get_pacientes_api(Request $request = null)
   {
     $pacientes_url = getenv('JSON_PLACE_HOLDER_URL') . '/pacientes/';
+    
+    if(!isset($request)) {
+      return $pacientes_url;
+    }
+
     $search_id = $request->id;
 
     return $pacientes_url . ($search_id ? "$search_id" : "");
+  }
+
+  /**
+   * Verifica si se han enviado correctamente los datos del usuario que se desea agregar,
+   * retorna una respuesta estructurada con el código 400 cuando no se envían los datos de manera
+   * adecuada o TRUE en caso de validar correctamente los datos
+   * 
+   * @return JsonResponse|bool
+   */
+  private function verify_add_new_paciente_parameters(Request $request)
+  {
+    $parameters = $request->all();
+    if (empty($parameters)) {
+      return $this->get_invalid_paciente_parameters_response();
+    }
+
+    return TRUE;
+  }
+
+  /**
+   * Retorna una respuesta estructurada en formato JsonResponse, se utiliza como
+   * respuesta cuando los datos enviados del paciente son inválidos
+   * 
+   * @return JsonResponse
+   */
+  private function get_invalid_paciente_parameters_response()
+  {
+    return new JsonResponse([
+      "msg" => "Los datos del paciente son incorrectos o no se han proporcionado"
+    ], Response::HTTP_BAD_REQUEST);
   }
 }
