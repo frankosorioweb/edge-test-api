462b0035f5c583776c4ba1ee279d734cb87c9b9d
frankosorioweb <osoriofranciscoj99@gmail.com>
2022-10-14 07:31:05 -0300

    El endpoint /generateToken retorna el api_key que será necesario para ejecutar cualquier servicio

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,19 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Este endpoint se utiliza para obtener el api_key que será necesario para
+ * poder ejecutar cualquier otro servicio de esta API, para poder ejecutar
+ * cualquier otro servicio, se debe enviar en el header la propiedad api_key
+ * con el respectivo valor que retorna este endpoint
+ * 
+ * @param Request
+ * @return JsonResponse
+ */
 Route::get('/generateToken', function(Request $request) {
+  $api_key = base64_encode(getenv("API_KEY"));
+
   return new JsonResponse([
-    "msg" => "Hello from /generateToken [GET]"
+    "api_key" => $api_key
   ]);
 });
